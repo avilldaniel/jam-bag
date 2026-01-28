675f6571a706bb694efad2b83a5ac13919aee3ea
Daniel Stream <90011911+avilldaniel@users.noreply.github.com>
2026-01-28 15:52:17 -0800

    chore: wip git commit pre-fix

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation only
+        'style', // Formatting, whitespace (not CSS)
+        'refactor', // Code change that neither fixes nor adds
+        'perf', // Performance improvement
+        'test', // Adding or updating tests
+        'build', // Build system or dependencies
+        'ci', // CI configuration
+        'chore', // Maintenance tasks
+        'revert', // Reverting a previous commit
+        'wip', // Work in progress
+      ],
+    ],
+  },
 }
